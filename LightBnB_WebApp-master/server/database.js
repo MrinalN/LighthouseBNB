d5c01413a660069f6420a6bc41642d6fe3126311
Adele Noronha <adele.c.noronha@gmail.com>
2021-04-01 20:59:58 +0000

    Add promise and query to getAllReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -82,7 +82,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  return pool.query(`
+  SELECT properties.*,reservations.*, avg(property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN properties ON properties.id = property_id
+  JOIN property_reviews ON reservation_id = reservations.id
+  WHERE reservations.guest_id = $1 
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY start_date
+  LIMIT $2;
+  `, values)
+    .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
